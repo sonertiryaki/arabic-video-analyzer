50a95faef1a7c2a9d4d187a4386162e9b206bb84
sonertiryaki <39697696+sonertiryaki@users.noreply.github.com>
2026-02-08 13:30:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request
-from drive import prepare_video_folder, download_video, upload_result, already_processed, mark_processed
+from drive import (
+    prepare_video_folder,
+    download_video_from_request,
+    upload_result,
+    already_processed,
+    mark_processed
+)
 from video import extract_frames
 from ocr import run_ocr
 from analysis import create_excel
@@ -10,25 +16,23 @@ app = Flask(__name__)
 @app.route("/process", methods=["POST"])
 def process():
 
-    file_id = request.json["fileId"]
+    # video dosyasını kaydet
+    video_file = download_video_from_request(request)
 
-    folder_id, filename = prepare_video_folder(file_id)
+    folder_name, filename = prepare_video_folder(video_file)
 
-    if already_processed(folder_id):
+    if already_processed(folder_name):
         return "already processed"
 
-    video = download_video(file_id)
-    frames = extract_frames(video)
-
+    frames = extract_frames(video_file)
     text = run_ocr(frames)
 
     report = create_excel(text)
 
-    upload_result(report, folder_id)
+    upload_result(report, folder_name)
+    mark_processed(folder_name)
 
-    mark_processed(folder_id)
-
-    send_mail(filename + " analiz edildi")
+    send_mail(f"{filename} analiz edildi")
 
     return "ok"
 
